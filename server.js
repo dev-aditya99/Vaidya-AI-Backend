98c797415f19004708c4546c7c4a10802ff657fa
dev-aditya99 <mail.adityasharma99@gmail.com>
2025-06-10 19:00:55 +0530

    Handling Errors

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const _dirname = path.resolve();
 app.use(express.json())
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["POST", "GET"]
 }));
